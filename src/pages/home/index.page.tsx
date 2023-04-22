39cd87f7a1a5fcc176205daaa9f491753dc2aba1
killer-cf <costa.kilder@live.com>
2023-04-22 11:26:39 -0300

    style: last reading box

--- src/pages/home/index.page.tsx
+++ src/pages/home/index.page.tsx
@@ -1,11 +1,74 @@
-import { Container, Content } from './styles'
+import Link from 'next/link'
+import Image from 'next/image'
+import { CaretRight, ChartLineUp, Star } from 'phosphor-react'
+
 import { SideBar } from '@/components/SideBar'
+import {
+  Bookshelf,
+  BoxLastReading,
+  Container,
+  Content,
+  Feed,
+  Header,
+  RecentReading,
+  RecentReviews,
+  Spotlight,
+} from './styles'
 
 export default function Home() {
   return (
     <Container>
       <SideBar />
-      <Content></Content>
+      <Content>
+        <Header>
+          <ChartLineUp size={32} />
+          <h1>Início</h1>
+        </Header>
+        <Bookshelf>
+          <Feed>
+            <RecentReading>
+              <header>
+                <h3>Sua última leitura</h3>
+                <Link href="">
+                  Ver todas <CaretRight />
+                </Link>
+              </header>
+
+              <BoxLastReading>
+                <Image
+                  src="/images/books/entendendo-algoritmos.png"
+                  alt=""
+                  width={108}
+                  height={152}
+                />
+                <div style={{ flex: 1 }}>
+                  <header>
+                    <p>Há 2 dias</p>
+                    <div>
+                      <Star size={16} weight="fill" />
+                      <Star size={16} weight="fill" />
+                      <Star size={16} weight="fill" />
+                      <Star size={16} weight="fill" />
+                      <Star size={16} />
+                    </div>
+                  </header>
+
+                  <h2>Entendendo Algoritmos</h2>
+                  <span>Aditya Bhargava</span>
+
+                  <p>
+                    Nec tempor nunc in egestas. Euismod nisi eleifend at et in
+                    sagittis. Penatibus id vestibulum imperdiet a at imperdiet
+                    lectu...
+                  </p>
+                </div>
+              </BoxLastReading>
+            </RecentReading>
+            <RecentReviews></RecentReviews>
+          </Feed>
+          <Spotlight></Spotlight>
+        </Bookshelf>
+      </Content>
     </Container>
   )
 }
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,7 +1,117 @@
 import { styled } from '../../../stitches.config'
 
-export const Container = styled('main', {
+export const Container = styled('div', {
+  display: 'flex',
+  maxWidth: '1440px',
+  margin: '0 auto',
+})
+
+export const Content = styled('main', {
+  flex: 1,
+  padding: '4rem 4.75rem 6rem',
+})
+
+export const Header = styled('header', {
+  marginBottom: '$10',
+  display: 'flex',
+  gap: '$3',
+
+  h1: {
+    color: '$gray100',
+    fontSize: '$2xl',
+    lineHeight: '$short',
+  },
+
+  svg: {
+    color: '$green100',
+  },
+})
+
+export const Bookshelf = styled('div', {
   display: 'flex',
 })
 
-export const Content = styled('div', {})
+export const Feed = styled('div', {
+  maxWidth: '38rem',
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+})
+
+export const RecentReading = styled('section', {
+  flex: 1,
+
+  '> header': {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: '$4',
+  },
+
+  h3: {
+    color: '$gray100',
+    fontSize: '$sm',
+    fontWeight: 400,
+    lineHeight: '$base',
+  },
+
+  a: {
+    color: '$purple100',
+    fontSize: '$sm',
+    fontWeight: 700,
+    lineHeight: '$base',
+    textDecoration: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    gap: '$2',
+  },
+})
+
+export const BoxLastReading = styled('div', {
+  backgroundColor: '$gray600',
+  display: 'flex',
+  padding: '$5 $6',
+  borderRadius: '$md',
+  gap: '$6',
+  alignItems: 'flex-start',
+
+  p: {
+    color: '$gray300',
+    fontSize: '$sm',
+    lineHeight: '$base',
+  },
+
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: '$3',
+
+    div: {
+      display: 'flex',
+      gap: '$1',
+    },
+
+    svg: {
+      color: '$purple100',
+    },
+  },
+
+  h2: {
+    color: '$gray100',
+    fontSize: '$md',
+    lineHeight: '$base',
+  },
+
+  span: {
+    display: 'inline-block',
+    color: '$gray400',
+    fontSize: '$sm',
+    lineHeight: '$base',
+    marginBottom: '$6',
+  },
+})
+
+export const RecentReviews = styled('section', {})
+
+export const Spotlight = styled('section', {})
